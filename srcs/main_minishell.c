1990cc962275510aed4b8424890a3938fceee428
Bducrocq <bducrocq42@gmail.com>
2022-06-21 18:14:18 +0200

    save

--- srcs/main_minishell.c
+++ srcs/main_minishell.c
@@ -5,25 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/20 16:01:31 by bducrocq          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/06/20 18:01:19 by bducrocq         ###   ########.fr       */
-=======
-/*   Updated: 2022/06/21 17:58:32 by bducrocq         ###   ########.fr       */
->>>>>>> main
+/*   Created: 2022/06/21 18:12:46 by bducrocq          #+#    #+#             */
+/*   Updated: 2022/06/21 18:14:12 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
 
 #include <../includes/minishell.h>
 
 int	main(int ac, char **av, char **env)
 {
 	printf("Hello World!\n");
-<<<<<<< HEAD
 	test_execve_ls_fork(av);
-
-=======
 	ft_putstr("Test libft !\n");
->>>>>>> main
 	return (0);
 }
