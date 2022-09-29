f2814371aabef8a5e614bfca8352f7c01053756d
Bducrocq <bducrocq42@gmail.com>
2022-09-29 17:37:23 +0200

    recup dbg get pid

---
+++ srcs/dbg/debug_execve.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug_execve.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 00:32:10 by bducrocq          #+#    #+#             */
+/*   Updated: 2022/09/29 16:28:35 by bducrocq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <./../includes/minishell.h>
+
+void dbg_fork_display_pid(const char *str) //TODO: getpid interdit, a virer avant push project
+{
+	printf("%spid = %i\n", str, getpid());
+}
