f4e8223b07dafccc2bbbf65d67ce153aa3d6af0d
Bducrocq <bducrocq42@gmail.com>
2022-10-07 23:12:51 +0200

    save

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 17:50:37 by hmarconn          #+#    #+#             */
-/*   Updated: 2022/10/07 22:11:32 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/10/07 23:01:08 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int			ft_check_if_cmd_has_pipe(t_list *lst);
 /* FT BUILTIN */
 void		ft_builtin_env(t_envlst *envlst);
 int			ft_builtin_export(t_envlst *env, char **cmd, t_data	*data);
-int			ft_builtin_unset(t_envlst *env, char **cmd);
+int			ft_builtin_unset(t_data *data, char **cmd);
 int			ft_builtin_cd(t_envlst *env, char **argv);
 int			ft_builtin_pwd(t_envlst *env, char **argv);
 int			ft_builtin_echo(char **argv);
--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,27 +6,27 @@
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:01:31 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/10/07 22:55:09 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/10/07 23:04:14 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/minishell.h"
 
-static int	ft_main_unset(t_envlst **env, char *key)
+static int	ft_main_unset(t_data *data, char *key)
 {
 	t_envlst	*tmp;
 	char		*ptr;
 
 	if (key[0] == '\0')
 		return (-1);
-	tmp = (*env);
-	if (ft_env_check_if_key_is_valid((*env), key) == FALSE)
+	tmp = data->env;
+	if (ft_env_check_if_key_is_valid(tmp, key) == FALSE)
 		return (0);
 	if (ft_strequal(key, "PWD") == TRUE)
 		return (ft_putstr_fd("failure: PWD is required\n", 2));
 	if (ft_strequal(tmp->key, key))
 	{
-		ft_env_lstclear_one_first_element(env);
+		ft_env_lstclear_one_first_element(&data->env);
 		return (0);
 	}
 	while (tmp && tmp->next)
@@ -48,11 +48,11 @@ static int	ft_main_unset(t_envlst **env, char *key)
  * @param cmd 
  * @return int 
  */
-int	ft_builtin_unset(t_envlst *env, char **cmd)
+int	ft_builtin_unset(t_data *data, char **cmd)
 {
 	if (!cmd[1] || cmd[1][0] == '\0')
 		return (1);
 	else
-		ft_main_unset(&env, cmd[1]);
+		ft_main_unset(data, cmd[1]);
 	return (0);
 }
