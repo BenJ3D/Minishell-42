387539e650cb403e19fca549462317cc2d35ed07
BenJ3D <benjamin@ben3d.fr>
2022-10-30 21:58:43 +0100

    save

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bducrocq <bducrocq@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:01:31 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/10/05 15:12:50 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/10/30 02:51:09 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	ft_builtin_cd(t_envlst *env, char **argv)
 {
 	// ft_putstr("cd: Work In Progress...\n");
+	//faire un acess
 	chdir(argv[1]);
 	return (0);
 }
