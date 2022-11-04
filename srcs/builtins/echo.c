d82dd3ec13b3396962d4a9eb2ca886de24647d6a
Bducrocq <bducrocq42@gmail.com>
2022-11-05 00:34:02 +0100

    OK et normé

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,22 +6,21 @@
 /*   By: bducrocq <bducrocq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 16:01:31 by bducrocq          #+#    #+#             */
-/*   Updated: 2022/11/05 00:03:39 by bducrocq         ###   ########.fr       */
+/*   Updated: 2022/11/05 00:32:49 by bducrocq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./../includes/minishell.h"
-
 
 int	ft_builtin_echo_check_option(char *str)
 {
 	int	iter;
 
 	iter = 0;
-	if ( str[iter] == '-' && str[iter + 1] == 'n')
+	if (str[iter] == '-' && str[iter + 1] == 'n')
 	{
 		iter += 1;
-		while(!ft_isspace(str[iter]) && str[iter])
+		while (!ft_isspace(str[iter]) && str[iter])
 		{
 			if (str[iter] != 'n')
 				return (0);
@@ -32,7 +31,33 @@ int	ft_builtin_echo_check_option(char *str)
 	return (0);
 }
 
-int	ft_builtin_echo(char **argv) //TODO: prendre en charge le flag -n  -nnnnn
+static int	ft_builtin_echo_norm(char **argv, int i, int previsop, int option)
+{
+	while (argv[i])
+	{
+		if (!ft_builtin_echo_check_option(argv[i]))
+		{
+			ft_putstr_fd(argv[i], 1);
+			previsop = 0;
+			if (argv[i + 1])
+				ft_putchar(' ');
+		}
+		else if (ft_builtin_echo_check_option(argv[i]) && previsop == 0)
+		{
+			ft_putstr_fd(argv[i], 1);
+			if (argv[i + 1])
+				ft_putchar(' ');
+		}
+		else
+			previsop = 1;
+		i++;
+	}
+	if (option == 0)
+		ft_putchar('\n');
+	return (0);
+}
+
+int	ft_builtin_echo(char **argv)
 {
 	int	i;
 	int	option;
@@ -51,22 +76,6 @@ int	ft_builtin_echo(char **argv) //TODO: prendre en charge le flag -n  -nnnnn
 		i = 2;
 	}
 	g_status = 0;
-	printf ("option = %i\n", option);
-	while (argv[i])
-	{
-		if (!ft_builtin_echo_check_option(argv[i]))
-		{
-			ft_putstr_fd(argv[i], 1);
-			if (argv[i + 1])
-				ft_putchar(' ');
-		}
-		else
-			previsop = 1;
-		if (argv[i + 1] && previsop == 0)
-			ft_putchar(' ');
-		i++;
-	}
-	if (option == 0)
-		ft_putchar('\n');
+	ft_builtin_echo_norm(argv, i, previsop, option);
 	return (0);
 }
